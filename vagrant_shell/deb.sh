b93664b0b8bd25b440e7a934a96058384df836f7
DataFibers Admin <datafibers@gmail.com>
2018-03-29 19:13:48 -0400

    Update deb.sh

--- vagrant_shell/deb.sh
+++ vagrant_shell/deb.sh
@@ -221,8 +221,9 @@ if [ "$install_hive" = true ]; then
 fi
 
 echo "Creating keys for passwordless ssh"
-echo -e  'y' | ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ''
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+echo -e  'y' | ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -P ''
+chmod 777 ~/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
 ENDTIME=$(date +%s)
