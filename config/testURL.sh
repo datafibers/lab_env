90032f2e216eeef526c024597365ca249bf57c69
vagrant <vagrant@vagrant.vm>
2018-05-13 02:41:31 +0000

    added testURL

---
+++ config/testURL.sh
@@ -0,0 +1,12 @@
+. install_version.sh
+
+for i in `(( set -o posix ; set ) | grep dl_link_ | cut -d "=" -f 2)`; do
+res=$(wget --spider $i -nv 2>&1)
+if [[ $res = *"OK"* ]]; then
+  echo $res
+  continue
+else
+  echo "$res is broken!!"
+  exit 1
+fi
+done
