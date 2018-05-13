55afdd29255e51259e932afda5a17e08ec09e02f
DataFibers Admin <datafibers@gmail.com>
2018-05-12 23:20:34 -0400

    Update install_version.sh

--- config/install_version.sh
+++ config/install_version.sh
@@ -46,7 +46,7 @@ file_name_confluent=confluent-4.1.0.tar.gz
 dl_link_confluent=http://packages.confluent.io/archive/4.1/confluent-oss-4.1.0-2.11.tar.gz
 
 file_name_flink=flink-1.3.2.tgz
-dl_link_flink=${apache_root_site}/flink/flink-1.3.2/flink-1.3.2-bin-hadoop26-scala_2.11.tgz
+dl_link_flink=${apache_root_site}/flink/flink-1.3.2/flink-1.3.2-bin-hadoop26-scala_2.11.tgzs
 
 file_name_elastic=elastic-2.3.4.tar.gz
 dl_link_elastic=https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.4/elasticsearch-2.3.4.tar.gz
