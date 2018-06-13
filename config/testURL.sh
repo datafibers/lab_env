c20432e9293d8171ae34ff140e65f6441d3a2969
DataFibers Admin <datafibers@gmail.com>
2018-06-13 18:16:53 -0400

    Update testURL.sh

--- config/testURL.sh
+++ config/testURL.sh
@@ -2,6 +2,7 @@ set -e
 . ./config/install_version.sh
 
 for i in `(( set -o posix ; set ) | grep dl_link_ | cut -d "=" -f 2)`; do
+echo "Verifying $i ..."
 res=$(wget --spider $i -nv --header="Cookie: oraclelicense=accept-securebackup-cookie" 2>&1)
 if [[ $res = *"OK"* ]]; then
   echo $res
