85a4aaa21f6f6dc2b58b067475daef60e2fb9f20
DataFibers Admin <datafibers@gmail.com>
2018-09-26 11:50:41 -0400

    Update ops.sh
    added kill KSQL

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -142,16 +142,22 @@ stop_confluent () {
 if [ -h ${DF_APP_DEP}/confluent ]; then
 	echo "[INFO] Shutdown [Schema Registry]"
 	schema-registry-stop ${DF_APP_CONFIG}/schema-registry.properties 1> ${DF_APP_LOG}/schema-registry.log 2> ${DF_APP_LOG}/schema-registry.log &
+	echo "[INFO] Shutdown [Apache Kafka SQL]"
+	ksql-server-stop ${DF_APP_CONFIG}/server.properties 1> ${DF_APP_LOG}/ksql.log 2> ${DF_APP_LOG}/ksql.log &
 	echo "[INFO] Shutdown [Apache Kafka Server]"
 	kafka-server-stop ${DF_APP_CONFIG}/server.properties 1> ${DF_APP_LOG}/kafka.log 2> ${DF_APP_LOG}/kafka.log &
 	sleep 15
 	sid=$(getSID ${KAFKA_DAEMON_NAME})
 	if [ ! -z "${sid}" ]; then
     	kill -9 ${sid}
-		echo "[WARN] Kafka PID is killed after 15 sec. time out."
+	echo "[WARN] Kafka PID is killed after 15 sec. time out."
+	sid=$(getSID ${KAFKA_KSQL_DAEMON_NAME})
+	if [ ! -z "${sid}" ]; then
+    	kill -9 ${sid}
+	echo "[WARN] KSQL PID is killed after 15 sec. time out." 
     fi
 	echo "[INFO] Shutdown [Apache Kafka Connect]"
-    sid=$(getSID ${KAFKA_CONNECT_DAEMON_NAME})
+        sid=$(getSID ${KAFKA_CONNECT_DAEMON_NAME})
 	if [ ! -z "${sid}" ]; then
     	kill -9 ${sid}
     fi
